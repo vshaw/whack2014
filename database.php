673ad72cf0bcbb3588d0cd86a7c5080dc82bfd64
Vivienne Shaw <vshaw@wellesley.edu>
2014-03-08 17:39:16 -0500

    database

--- database.php
+++ database.php
@@ -1,13 +1,18 @@
 <?php
-    $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-    $server = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $db = substr($url["path"],1);
-
-    mysql_connect($server, $username, $password);
-
-
-    mysql_select_db($db);
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+function pg_connection_string() {
+  return "dbname=dcm3jsrdb6tfv9 host=ec2-54-204-43-138.compute-1.amazonaws.com port=54
+32 user=usxfrhbbogxcra password=LdapB749U7QIfJkQgPW20CpvEh sslmode=require";
+}
+ 
+# Establish db connection
+$db = pg_connect(pg_connection_string());
+if (!$db) {
+    echo "Database connection error."
+    exit;
+}
+ 
+$result = pg_query($db, "SELECT statement goes here");
+echo "<h2>worked";
 ?>
